e21deb93af547624886b74d3ce47265f7d9592ae
Morgan Leef <mlleef@mix.wvu.edu>
2018-10-16 15:24:25 -0400

    Added code that creates the matrix with only data

--- JamesL_code.R
+++ JamesL_code.R
@@ -1,11 +1,14 @@
 setwd ("~/desktop/ESDA/GitHub_Ex/GitHubEx2")
 
-library(xl)
+library(readxl)
 
+#Load JL's first file "Matrices 461-470(1).xlsx" into R
 x_data <- read_xlsx("Matrices 461-470(1).xlsx")
 
-sheets <- excel_sheets(x_data)
+#Create a vector with the names of each sheet from "x_data"
+sheets <- excel_sheets("Matrices 461-470(1).xlsx")
 
-data_tb <- matrix()
+#Create a matrix with only the numerical data values from "x_data"
+data_tb <- x_data[2:55,3:22]
 
 data_x[data_x$Ancestor == 1, "Ancestor"] <- 0
