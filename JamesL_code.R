678f3241ddc14c7e1669b0a1528b04b1f266ae50
Morgan Leef <mlleef@mix.wvu.edu>
2018-10-16 17:21:59 -0400

    Added code that accomplishes everything but part D

--- JamesL_code.R
+++ JamesL_code.R
@@ -9,6 +9,13 @@ x_data <- read_xlsx("Matrices 461-470(1).xlsx")
 sheets <- excel_sheets("Matrices 461-470(1).xlsx")
 
 #Create a matrix with only the numerical data values from "x_data"
-data_tb <- x_data[2:55,3:22]
+data_tb <- x_data[2:56,3:22]
 
-data_x[data_x$Ancestor == 1, "Ancestor"] <- 0
+#Extract the species names into their own vector
+species <- x_data[2:55,2]
+
+#Create a new "group" vector that contains repeats for all groups
+group <- rep((c(3,1) times = 3), (c(6,1) times = 16), (c(22,1) times = 3), (c(25,1) times = 4), (c(29,1) times = 9), (c(38,1) times = 19))
+
+#Exracting the first column from the matrix and naming it "V1"
+V1 <- data_tb[,1]
