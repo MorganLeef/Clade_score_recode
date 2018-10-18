fee1483eed1aa3bc27093998aab960d8fd3e9542
Amanda Adams <aeadams@mix.wvu.edu>
2018-10-17 23:13:54 -0400

    Added the unname fuction and redid some of the comments.

--- JamesL_code.R
+++ JamesL_code.R
@@ -1,8 +1,8 @@
-#R script that reads JL's excel file and extracts the first column. 
+#R script that reads JL's excel file and extracts information including first column, species name, and group name.    
 #Sets up the working directory.
 setwd ("~/desktop/ESDA/GitHub_Ex/GitHubEx2")
 
-#Adds the readxl to the library. So that it can be used for the script.
+#Adds the readxl to the library. 
 library(readxl)
 
 #Load JL's first file "Matrices 461-470(1).xlsx" into R
@@ -17,14 +17,17 @@ data_tb <- x_data[2:56,3:22]
 #Extract the species names into their own vector
 species <- x_data[2:55,2]
 
-#Extract the names for the group and unlist makes tbl not a list
-Names<-unlist(x_data[1:55,1]) 
+#Extract the names for the group and unlist function makes tbl not a list. 
+names<-unlist(x_data[1:55,1]) 
 
 #Got rid of the N/A values in the column.
-Groupnames<-na.omit(Names)
+groupnames<-na.omit(names)
 
 #This creates a vector named groups to have all of the groupnames from column 1 of x_data.   
-groups <- rep(Groupnames, c(3,16,3,4,9,19))
+rep.groups <- rep(groupnames, c(3,16,3,4,9,19))
+
+#This code takes away the X__* name from the rep.groups vector.  
+groups<-unname(rep.groups)
 
 #Exracting the first column from the matrix and naming it "V1"
 V1 <- data_tb[,1]
