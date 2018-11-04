220c8961ad008566e4f7a0845551e6a54b0f2ca1
Blackcat56 <43677736+Blackcat56@users.noreply.github.com>
2018-11-04 14:34:17 -0500

    changed it to be amys code
    Since our didnt work. I changed it to hers.

--- JamesL_code.R
+++ JamesL_code.R
@@ -1,58 +1,34 @@
-#R script that reads JL's excel file and extracts information including first column, species name, and group name.   
+#R script that recodes all the matrix into what is needed. 
 
-#Adds the readxl to the library. 
-library(readxl)
+rm(list=ls())
 
-#Load JL's first file "Matrices 461-470(1).xlsx" into R
-x_data <- read_xlsx("Matrices 461-470(1).xlsx")
+library(readxl) #included in tidyverse
+library(tidyverse)
 
-#Create a vector with the names of each sheet from "x_data"
-sheets <- excel_sheets("Matrices 461-470(1).xlsx")
+#Read in one of Lamsdell's excel files and reshape
+xfile_name <- "Matrices 461-470.xlsx"
 
-#Create a matrix with only the numerical data values from "x_data"
-data_tb <- x_data[2:56,3:22]
+x_data <- read_excel(xfile_name)
+sheet_name <- excel_sheets(xfile_name)
+n_sheets <- length(sheets)
+species <- rbind(x_data[2:55,2], "ancestor")
+group <- pull((fill(x_data, 1, .direction = "down")[1:(nrow(x_data)-1),]), var = 1)
 
-#Extract the species names into their own vector
-species <- x_data[2:55,2]
+data_tb <- read_excel(xfile_name, sheet=sheet_name[1], range="R4C3:R58C22", 
+                      col_names = FALSE)
 
-#Load "tidyr" package to use "fill"
-library(tidyr)
+v1 <- pull(data_tb, X__7) #creates a single vector from the tbl
+####Becomes Function paleo_f.R
 
-#Use "fill" to occupy empty cells with group names
-group <- x_data %>% fill(3,1)
+    if (v1[55]==1) {
+      v1[1:54][v1[1:54]==1 ] <- -888 #will be changed to 0
+    } else {if (v1[55]==2)
+      v1[1:54][v1[1:54]==2 ] <- -777 #will be changed to 0
+    }
 
-#Exracting the first column from the matrix and naming it "V1"
-V1 <- data_tb[,1]
+v1[1:54][v1[1:54]==0] <- v1[55] #replace all 0s with value in last row (ancestor)
 
-#James L Problem Part 2
+v1[1:54][v1[1:54]==-888] <- 0
+v1[1:54][v1[1:54]==-777] <- 0
+v1[1:54][v1[1:54]==2] <- -1
 
-library("dplyr")
-
-#Changes the column header "1" to "A" so it can be called.
-colnames(V1)[colnames(V1)=="1"] <- "A"
-
-#Checks row 55 for 1's and 2's, and if present, changes the values of the entire column to 0.
-if(V1 [55,] == 1){
-  V1$A<-recode(V1$A,"1='0'")
-} else if(V1 [55,] == 2){
-  V1$A<-recode(V1$A,"2='0'")
-}
-
-#Checks for existing zeroes, and if present, changes the zeroes to whatever value is in template row 55
-if(V1 [55,1] != 0){
-  V1$A<-recode(V1$A,"0='[55,1]'")
-}
-
-#Changes all remaining twos to -1
-if(V1 [,] == 2){
-  V1$A<-recode(V1$A,"2='1'")
-}
-  
-
-
-
-
-#crap
-V1$A<-recode(V1$A,"c(1,2)='0'")
-#kinda works
-V1[55,1] <- ifelse(V1[55,1] == "2", 0, ifelse(V1[55,1] == "1", 0, ifelse(V1[55,1] == "0", 0)))
