7c52cafd18be206956d4ab9234c1c6116234abe6
Blackcat56 <43677736+Blackcat56@users.noreply.github.com>
2018-11-04 21:30:08 -0500

    changed Amy's code so it would work for me.
    The tb is a pain in the ###.

--- JamesL_code.R
+++ JamesL_code.R
@@ -6,29 +6,21 @@ library(readxl) #included in tidyverse
 library(tidyverse)
 
 #Read in one of Lamsdell's excel files and reshape
-xfile_name <- "Matrices 461-470.xlsx"
-
-x_data <- read_excel(xfile_name)
-sheet_name <- excel_sheets(xfile_name)
+x_data <- read_xlsx("Matrices 461-470(1).xlsx")
+sheets <- excel_sheets("Matrices 461-470(1).xlsx")
 n_sheets <- length(sheets)
-species <- rbind(x_data[2:55,2], "ancestor")
-group <- pull((fill(x_data, 1, .direction = "down")[1:(nrow(x_data)-1),]), var = 1)
+group <- x_data %>% fill(3,1)
+v1vec<-pull (v1)
 
-data_tb <- read_excel(xfile_name, sheet=sheet_name[1], range="R4C3:R58C22", 
-                      col_names = FALSE)
+if (v1vec[55]==1) {
+  v1vec[1:54][v1vec[1:54]==1 ] <- -888 #will be changed to 0
+} else {if (v1vec[55]==2)
+  v1vec[1:54][v1vec[1:54]==2 ] <- -777 #will be changed to 0
+}
 
-v1 <- pull(data_tb, X__7) #creates a single vector from the tbl
-####Becomes Function paleo_f.R
+v1vec[1:54][v1vec[1:54]==0] <- v1vec[55] #replace all 0s with value in last row (ancestor)
 
-    if (v1[55]==1) {
-      v1[1:54][v1[1:54]==1 ] <- -888 #will be changed to 0
-    } else {if (v1[55]==2)
-      v1[1:54][v1[1:54]==2 ] <- -777 #will be changed to 0
-    }
-
-v1[1:54][v1[1:54]==0] <- v1[55] #replace all 0s with value in last row (ancestor)
-
-v1[1:54][v1[1:54]==-888] <- 0
-v1[1:54][v1[1:54]==-777] <- 0
-v1[1:54][v1[1:54]==2] <- -1
+v1vec[1:54][v1vec[1:54]==-888] <- 0
+v1vec[1:54][v1vec[1:54]==-777] <- 0
+v1vec[1:54][v1vec[1:54]==2] <- -1
 
