e3889ba611b76ea19c8e2ca2403d02b7a116b2c6
Morgan Leef <mlleef@mix.wvu.edu>
2018-11-06 17:15:56 -0500

    Made finishing touches to source and apply R script

--- rolling_function.R
+++ rolling_function.R
@@ -1,6 +1,8 @@
+#This new R script sources the code from James L's original code, and uses apply to run the function changing the valuse for any specified column in the matrix
 rm(list=ls())
 
-
+#Source code and function from James L R script
 source("JamesL_code.R")
 
-coded_m <- apply (data_tb, runs)
+#Apply this function to any specified column
+coded_m <- apply(data_tb2, 2, v1_fun)
