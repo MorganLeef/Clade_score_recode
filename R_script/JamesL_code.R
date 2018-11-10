0043d6c64fa78fee8b4846d6a80ff5ae96f671ba
autumdowney <ardowney@mix.wvu.edu>
2018-11-10 15:15:43 -0500

    applied loop to all sheets in all files

--- R_script/JamesL_code.R
+++ R_script/JamesL_code.R
@@ -10,28 +10,26 @@ library(tidyr)
 
 files <- list.files("~/Desktop/ESDA/exercises/JLR_project/raw_data")
 
-for (x in files) {
-
+for (i in files) {
 #Read in one of Lamsdell's excel files and reshape
-xfile_name <- x
+xfile_name <- i
 
 #Extracts the read in data into different vectors. 
 x_data <- read_excel(xfile_name)
 sheet_name <- excel_sheets(xfile_name)
 species <- rbind(x_data[2:55,2], "ancestor")
 group <- pull((fill(x_data, 1, .direction = "down")[1:(nrow(x_data)-1),]), var = 1)
-
+for (x in sheet_name) {
 data_tb <- read_excel(xfile_name, sheet=sheet_name[1], range="R4C3:R58C22",col_names = FALSE)
 
 orig_column <- pull(data_tb, X__1) #creates a single vector from the tbl
-####Becomes Function paleo_f.R
-#Creates a function that changes values based on James L desires for each column
-#This new R script sources the code from James L's original code, and uses apply to run the function changing the valuse for any specified column in the matrix
+
 
 #Source code and function from James L R script
 source("recoding_column_function.R")
 
-assign(paste0((strsplit(x,"-")[[1]]),"d"), apply(data_tb, 2, recoding_fun))
-}
+assign(paste0((x), "d"), apply(data_tb, 2, recoding_fun))
+}}
+
 
 
