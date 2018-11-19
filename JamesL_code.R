5e64dbcb07458740eba6f3030e40d13da6f59eb4
Blackcat56 <43677736+Blackcat56@users.noreply.github.com>
2018-11-19 11:08:42 -0500

    Updated the code!

--- JamesL_code.R
+++ JamesL_code.R
@@ -1,38 +1,42 @@
-#Simple solution for one and three cols of JL data
+
+#Deletes current working enviroment. 
 rm(list=ls())
-
-library(readxl) #included in tidyverse
+#load packages needed for script. 
+library(readxl) 
 library(tidyverse)
+library(tidyr)
 
-#Read in one of Lamsdell's excel files and reshape
-xfile_name <- "Matrices 461-470(1).xlsx"
+#Provides path to Matrices files, and creates an object
 
-x_data <- read_excel(xfile_name)
-sheet_name <- excel_sheets(xfile_name)
-n_sheets <- length(sheets)
-species <- rbind(x_data[2:55,2], "ancestor")
-group <- pull((fill(x_data, 1, .direction = "down")[1:(nrow(x_data)-1),]), var = 1)
+files <- list.files("./raw_data")
 
-data_tb2 <- read_excel(xfile_name, sheet=sheet_name[1], range="R4C3:R58C22", 
-                      col_names = FALSE)
 
-v1 <- pull(data_tb2, X__2) #creates a single vector from the tbl
-####Becomes Function paleo_f.R
-v1_fun<-function(runs){
+
+#Loops code that partitions each file for reformatting in preparation for the recoding function
+for (xfile_name in files) {
+
+  x_data <- read_excel(paste0("R_script/", xfile_name)) #you are not in your WD
+  sheet_name <- excel_sheets(paste0("R_script/", xfile_name)) #you are not in your WD
+  species <- rbind(x_data[2:55,2], "ancestor")
+  group <- pull((fill(x_data, 1, .direction = "down")[1:(nrow(x_data)-1),]), var = 1)
+
+    for (x in sheet_name) {
+    data_tb <- read_excel(paste0("R_script/", xfile_name), sheet=x[1], range="R4C3:R58C22",col_names = FALSE)
+    
+    orig_column <- pull(data_tb, X__1) 
   
-if (runs[55]==1) {
-    runs[1:54][runs[1:54]==1 ] <- -888 #will be changed to 0
-} else {if (runs[55]==2)
-    runs[1:54][runs[1:54]==2 ] <- -777 #will be changed to 0
-}
+    #Source recoding column function that recodes values in a matrix
+    source("recoding_column_function.R")
+    
+    #Applies the recoding function to each matrix
+    assign(paste0((x), "d"), apply(data_tb, 2, recoding_fun))
+    dataframe_list = grep("\\w+\\s\\w+d", ls(), value=T)
+    }
+  }
 
-runs[1:54][runs[1:54]==0] <- runs[55] #replace all 0s with value in last row (ancestor)
+#Calculates scores and weighted scores (means for each species (total of values divided by the number of columns))
+for (y in dataframe_list) {
+assign(y, (cbind(get(y), "rowMeans"=rowMeans(get(y)))))
+assign(y, cbind(species, get(y)))
+ }
 
-runs[1:54][runs[1:54]==-888] <- 0
-runs[1:54][runs[1:54]==-777] <- 0
-runs[1:54][runs[1:54]==2] <- -1
-
-return(runs)
-
-}
-v1_fun(runs=v1)
