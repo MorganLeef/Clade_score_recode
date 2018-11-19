ac4188db1ebeff5104cd24ab100f1e3fa2ad34a6
Blackcat56 <43677736+Blackcat56@users.noreply.github.com>
2018-11-19 16:22:27 -0500

    I just changed the order to make it change the matrix.

--- R_script/JamesL_code.R
+++ R_script/JamesL_code.R
@@ -43,7 +43,7 @@ summary <- cbind(summary,v)
 summary <- summary [,-1]
 colnames(summary) <- dataframe_list
 #added the species to the summery dataset
-summary<-cbind(summary,species)
+summary<-cbind(species,summary)
 
 
 
